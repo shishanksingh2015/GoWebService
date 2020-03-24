c688222c90de143d9be0a50be0f5a15c022686b1
shishank <shishank@pulseid.com>
2020-03-24 17:21:31 +0530

    learned constants

--- learnvariables/pointers.go
+++ learnvariables/pointers.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-
+	const url = "www.darkforce.com"
+	const c int = 2
 	var firstPtr *string = new(string)
 	*firstPtr = "Yoda"
 	fmt.Println(*firstPtr)
@@ -14,4 +15,7 @@ func main() {
 	fmt.Println(ptr, *ptr)
 	secondName = "Darth Vader"
 	fmt.Println(ptr, *ptr)
+	fmt.Println(url)
+	fmt.Println(float32(c) + 4.2)
+	fmt.Println(c + 4)
 }
